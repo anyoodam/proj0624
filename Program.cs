b64e5f0c8cc0b8601c160e9808988f8ed6e647fd
yoodam <nonosangmin@gmail.com>
2021-06-24 15:46:25 +0900

    0622

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+
+namespace firstproj
+{
+    class Program 
+
+    {
+        static int numA =99;
+
+        string chA = "abcde";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello bts!");
+            // Console.WriteLine("" + numA);
+            //{}
+           // for (int i = 0; i < 10; i++)
+            //    Console.WriteLine(numA + i);
+            //}   
+            Program proA = new Program();
+            proA.doThat();
+
+       
+        }
+         void doThat()
+        {
+            Console.WriteLine(chA);
+         }
+    }
+}           
